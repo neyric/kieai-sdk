078928561ede18d5068aa440601e67ec0a5338ad
neyric <finegn@outlook.com>
2025-09-07 16:31:00 +0800

    Refactor index.ts to standardize import/export syntax
    - Updated import and export statements to use double quotes for consistency across the file.
    - Ensured all module exports maintain a uniform style, enhancing code readability and maintainability.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-export { KieAISDK } from './core/KieAISDK';
-export { HttpClient } from './core/HttpClient';
-export { BaseModule } from './core/BaseModule';
+export { KieAISDK } from "./core/KieAISDK";
+export { HttpClient } from "./core/HttpClient";
+export { BaseModule } from "./core/BaseModule";
 
-export { GPT4oImageModule } from './modules/GPT4oImageModule';
-export { FluxKontextModule } from './modules/FluxKontextModule';
-export { MidjourneyModule } from './modules/MidjourneyModule';
-export { RunwayModule } from './modules/RunwayModule';
-export { Veo3Module } from './modules/Veo3Module';
+export { GPT4oImageModule } from "./modules/GPT4oImageModule";
+export { FluxKontextModule } from "./modules/FluxKontextModule";
+export { MidjourneyModule } from "./modules/MidjourneyModule";
+export { RunwayModule } from "./modules/RunwayModule";
+export { Veo3Module } from "./modules/Veo3Module";
 
-export * from './types/common';
-export * from './types/errors';
-export * from './types/modules/gpt-image';
-export * from './types/modules/flux-kontext';
-export * from './types/modules/midjourney';
-export * from './types/modules/runway';
-export * from './types/modules/veo3';
+export * from "./types/errors";
+export * from "./types/modules/gpt-image";
+export * from "./types/modules/flux-kontext";
+export * from "./types/modules/midjourney";
+export * from "./types/modules/runway";
+export * from "./types/modules/veo3";
 
-import { KieAISDK } from './core/KieAISDK';
+import { KieAISDK } from "./core/KieAISDK";
 export default KieAISDK;
