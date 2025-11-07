ea333d48a83c4aa1dbbb04c2125a5b6545c659c3
neyric <finegn@outlook.com>
2025-11-07 23:56:27 +0800

    records. Bump version to 1.0.2 and add new plugins
    Add GrokImagine, Hailuo23, Kling25, and Wan25 plugins to the

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,18 @@ export type { SeeDanceV1API } from "./plugins/seedance-v1";
 export { GoogleVeo3Plugin } from "./plugins/google-veo3";
 export type { GoogleVeo3API } from "./plugins/google-veo3";
 
+export { GrokImaginePlugin } from "./plugins/grok-imagine";
+export type { GrokImagineAPI } from "./plugins/grok-imagine";
+
+export { Hailuo23Plugin } from "./plugins/hailuo-2-3";
+export type { Hailuo23API } from "./plugins/hailuo-2-3";
+
+export { Kling25Plugin } from "./plugins/kling-2-5";
+export type { Kling25API } from "./plugins/kling-2-5";
+
+export { Wan25Plugin } from "./plugins/wan-2-5";
+export type { Wan25API } from "./plugins/wan-2-5";
+
 // ========== 默认导出 ==========
 import { KieAISDK } from "./core/sdk";
 export default KieAISDK;
